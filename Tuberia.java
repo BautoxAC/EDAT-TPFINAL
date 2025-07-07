24f3f4c96cab51d2fd83313618cfaa24756af2a6
kameldayoub <kamel.dayoub.epet20@gmail.com>
2025-07-07 18:01:19 -0300

    Se completo TDA tuberia

--- Tuberia.java
+++ Tuberia.java
@@ -1,4 +1,13 @@
 package TrabajoFinal;
+/*
+De las tuberías se guarda la siguiente información:
+Nomenclatura
+Formato: Nomenclatura ciudad desde-Nomenclaturaciudad hasta, por ejemplo: si las ciudades son Neufuén y Cupral-Có sería NE3001-CC3002
+Caudal mínimo en metros cúbicos por hora
+Caudal máximo en metros cúbicos por hora
+Diámetro de la tubería en milímetros
+Estado: ACTIVO, EN REPARACIÓN, EN DISEÑO, INACTIVO.
+*/
 
 public class Tuberia {
 
@@ -7,5 +16,41 @@ public class Tuberia {
     private int caudalMaximo;
     private int diametroTuberia;
     private String estado;
+    
+    public Tuberia(String nomen,int cMax,int cMin, int diamTub, String est){
+        nomenclatura=nomen;
+        caudalMaximo=cMax;
+        caudalMinimo=cMin;
+        diametroTuberia=diamTub;
+        estado=est;
+    }
+
+    //Observadores
+    public int getCaudalMaximo() {
+        return caudalMaximo;
+    }
+    public int getCaudalMinimo() {
+        return caudalMinimo;
+    }
+    public int getDiametroTuberia() {
+        return diametroTuberia;
+    }
+    public String getEstado() {
+        return estado;
+    }
+    public String getNomenclatura() {
+        return nomenclatura;
+    }
+
+    //Modificadores
+    public void setCaudalMinimo(int caudalMinimo) {
+        this.caudalMinimo = caudalMinimo;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    //Formato: Nomenclatura ciudad desde-Nomenclaturaciudad hasta, por ejemplo: si las ciudades son Neufuén y Cupral-Có sería NE3001-CC3002
+
 
 }
