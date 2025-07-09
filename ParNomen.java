e7f97a6bf19db0136c862f5fae166f55897247f1
kameldayoub <kamel.dayoub.epet20@gmail.com>
2025-07-09 17:31:03 -0300

    Se agrego ParNomen.java

---
+++ ParNomen.java
@@ -0,0 +1,9 @@
+public class ParNomen {
+    private String nomenSalida;
+    private String nomenEntrada;
+    
+    ParNomen(String salida, String entrada){
+        nomenSalida = salida;
+        nomenEntrada = entrada;
+    }
+}
